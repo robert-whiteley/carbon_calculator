f15bc552d7b20e49c97a70f54fa1b7d788386531
Braveen Nagendram <n.braveen@gmail.com>
2024-03-10 23:37:00 +0000

    different versions of video/bounding box rendering and popups

--- carb_calc/api/templates/static/script2.js
+++ carb_calc/api/templates/static/script2.js
@@ -34,66 +34,72 @@ if (navigator.mediaDevices.getUserMedia) {
     .catch(function (err0r) {});
 }
 
-const FPS = 5;
+const FPS = 10;
 setInterval(() => {
   width = video.width;
   height = video.height;
   context.drawImage(video, 0, 0, width, height);
   var data = canvas.toDataURL("image/jpeg", 0.5);
-  if (socketActive) {
-    socket.emit("image", data);
-  }
+  socket.emit("image", data);
 }, 1000 / FPS);
 
   socket.on("result", function(result){
     if (socketActive){
     console.log(result['bboxes'])
     if (result['bboxes'].length >0){
-        console.log(result['bboxes'][0]['class'])
-        x = result['bboxes'][0]['bbox'][0]
-        y = result['bboxes'][0]['bbox'][1]
-        if (x<0){
-          x=0
+      result['bboxes'].forEach(function(obj){
+        console.log(obj['class'])
+        x = obj['bbox'][0]
+        y = obj['bbox'][1]
+        if (x < 0){
+          x = 0
         }
-        if (y<0){
-          y=0
+        if (y < 0){
+          y = 0
         }
 
-        box_width = result['bboxes'][0]['bbox'][2]*width - x
-        box_height = result['bboxes'][0]['bbox'][3]*height - y
-        x = x*width
-        y = y*height
-        console.log(x,y, box_width, box_height)
+        box_width = obj['bbox'][2] * width - x
+        box_height = obj['bbox'][3] * height - y
+        x = x * width
+        y = y * height
+        console.log(x, y, box_width, box_height)
 
-        context.strokeStyle = 'green';
+        var color;
+        switch(obj['class']) {
+          case 'person':
+            color = 'red';
+            break;
+          case 'remote':
+            color = 'blue';
+            break;
+          default:
+            color = 'green';
+        }
+
+        // Draw bounding box
+        context.strokeStyle = color;
         context.strokeRect(x, y, box_width, box_height);
-        context.linewidth = 5;
+        context.lineWidth = 1;
         context.fillStyle = "rgba(195, 255, 104, 0.1)";
         context.fillRect(x, y, box_width, box_height);
 
-        // Draw background for the label text
-        context.fillStyle = 'green'; // Background color for the text
-        const textWidth = context.measureText(result['bboxes'][0]['class']).width;
-        context.fillRect(x, y, textWidth + 10, 20);
-
-        // Draw label text
-        context.fillStyle = 'white';
-        context.font = '14px Verdana';
-        context.fillText(result['bboxes'][0]['class'], x + 5, y + 15);
-
+        // Draw label text on top corner of the bounding box
+        context.fillStyle = color;
+        context.font = '14px Arial';
+        context.fillText(obj['class'], x+2, y + 14);
         // Extract the class and carbon footprint per unit
-        var detectedClass = result['bboxes'][0]['class'];
-
+        var detectedClass = obj['class'];
         // Populate the popup modal with the extracted information
         var popupContent = document.getElementById('popupContent');
         popupContent.innerHTML = `
-          <img src="" alt="Cropped Image">
-          <p>Class: ${detectedClass}</p>
+        <img src="" alt="Cropped Image">
+        <p>Class: ${detectedClass}</p>
         `;
+      });
 
-        // Show the popup modal
-        modal.style.display = "block";
-        socketActive = false;
+      // Show the popup modal
+      //modal.style.display = "block";
+      //socketActive = false;
     }
   }
 })
